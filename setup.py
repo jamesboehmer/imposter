dcd6138defd3023d2ea40ec33971fa7ce9fb5c18
James Boehmer <james.boehmer@mlb.com>
2017-02-21 17:49:19 -0500

    Include logging.ini

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='imposter',
       url='https://github.com/jamesboehmer/imposter',
       packages=find_packages(),
       py_modules=['imposter'],
+      include_package_data=True,
       install_requires=['Flask==0.12', 'gunicorn==19.6.0', 'boto==2.45.0', 'netifaces==0.10.5'],
       entry_points={
           'console_scripts': [
